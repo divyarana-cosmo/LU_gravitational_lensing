5f930578b0f03ce12aed059c3e77002f4699794d
divyarana-cosmo <divyarana757@gmail.com>
2025-10-06 21:17:03 +0200

    updated

--- source/conf.py
+++ source/conf.py
@@ -6,26 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'AstroTwinColo_2024'
-copyright = '2024, Divya Rana and Navin Chaurasiya'
-author = 'Divya Rana and Navin Chaurasiya'
-release = '2024'
+project = 'Weak gravitational lensing, Leiden'
+copyright = '2025, Divya Rana'
+author = 'Divya Rana'
+release = '2025'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['nbsphinx', 'sphinx_rtd_theme']
-
-exclude_patterns = ['**.ipynb_checkpoints']
-
+#extensions = []
 
 templates_path = ['_templates']
 #exclude_patterns = []
+extensions = ['nbsphinx', 'sphinx_rtd_theme']
+
+#exclude_patterns = ['**.ipynb_checkpoints']
+
+
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = "sphinx_rtd_theme"
-#html_theme = 'alabaster'
+
+html_theme = 'alabaster'
 html_static_path = ['_static']
